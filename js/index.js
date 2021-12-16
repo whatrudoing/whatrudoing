ae941b71f5fa99db3838a85192f573c347884e25
whatrudoing <94329568+whatrudoing@users.noreply.github.com>
2021-12-16 11:24:57 +0900

    main page header logo scroll 시, 색상 변경 및 전체 header transition

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 var headers = document.querySelectorAll(".header-list");
 const container = document.querySelector(".container");
+var logo = document.querySelector(".header-logo");
 
 headers.forEach(function(header){
   container.addEventListener('scroll', function(){
     console.log(container.scrollTop)
     if (container.scrollTop >=0) {
       header.style.color = 'black'
+      logo.style.color = 'black'
     }
     if (container.scrollTop >=750) {
       header.style.color = '#ffff'
+      logo.style.color = 'white'
     }
     if (container.scrollTop >=1550) {
       header.style.color = 'black'
+      logo.style.color = 'black'
     }
   })
 }) 
