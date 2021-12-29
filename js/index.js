947a97cc9d373b586da34cba184822f1c2ba53fc
whatrudoing <94329568+whatrudoing@users.noreply.github.com>
2021-12-29 10:19:54 +0900

    전반적인 코드 정리

--- js/index.js
+++ js/index.js
@@ -2,20 +2,3 @@ var headers = document.querySelectorAll(".header-list");
 const container = document.querySelector(".container");
 var logo = document.querySelector(".header-logo");
 
-headers.forEach(function(header){
-  container.addEventListener('scroll', function(){
-    console.log(container.scrollTop)
-    if (container.scrollTop >=0) {
-      header.style.color = 'black'
-      logo.style.color = 'black'
-    }
-    if (container.scrollTop >=750) {
-      header.style.color = '#ffff'
-      logo.style.color = 'white'
-    }
-    if (container.scrollTop >=1550) {
-      header.style.color = 'black'
-      logo.style.color = 'black'
-    }
-  })
-}) 
