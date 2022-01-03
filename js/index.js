4d52e2b4623ecefe9418a084ba991cb36cfb701d
whatrudoing <94329568+whatrudoing@users.noreply.github.com>
2022-01-03 11:55:35 +0900

    main page words to move

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 var headers = document.querySelectorAll(".header-list");
 const container = document.querySelector(".container");
 var logo = document.querySelector(".header-logo");
+var dragTargets = document.querySelectorAll(".drag-box");  // 5개의 div
+var dragStartX, dragStartY; var objInitLeft, objInitTop;
+var inDrag = false;
 
 headers.forEach(function(header){
   container.addEventListener('scroll', function(){
@@ -19,3 +22,47 @@ headers.forEach(function(header){
     }
   })
 }) 
+
+// function drag_handler(event) {
+//   //  ondrag =  드래그할때 동작 
+//       console.log("Drag");
+//       console.log(event.target.getAttribute("id"))
+//       currentDraggingID = event.target.getAttribute("id") 
+//   }
+//   function dragover_handler(event) {
+//     //ondragover = draggable 엘리먼트가 drop영역위에 올라가면 
+//      console.log("dragOver");
+//      event.preventDefault();
+//   }
+          
+//   function drop_handler(event) {
+//     //ondrop = draggable 엘리먼트를 drop영역위에 떨어트리면
+//      console.log("droooop!");
+     
+//      document.getElementsByClassName("drag-box")[currentDraggingID].style.top=event.layerY+"px";
+//      document.getElementsByClassName("drag-box")[currentDraggingID].style.left=event.layerX+"px";
+//       event.preventDefault();
+//   }
+
+  // 5개
+  dragTargets.forEach(function(dragTarget) {
+    dragTarget.addEventListener('mousedown', function(e) {
+      e.preventDefault();
+      inDrag = true;
+      objInitLeft = dragTarget.offsetLeft; objInitTop = dragTarget.offsetTop;
+      dragStartX = e.pageX; dragStartY = e.pageY;
+    });
+
+    dragTarget.addEventListener('mousemove', function(e) {
+      e.preventDefault();
+      if (!inDrag) {return;}
+      dragTarget.style.left = (objInitLeft + e.pageX-dragStartX) + "px";
+      dragTarget.style.top = (objInitTop + e.pageY-dragStartY) + "px";
+    });
+
+    dragTarget.addEventListener('mouseup', function(e) {
+      e.preventDefault();
+      inDrag = false;
+    });
+
+  });
